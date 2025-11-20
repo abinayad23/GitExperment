ecbc6af576397d2cefca5152bc7f3a9b498d1485
abinayad23 <abinayadurai152@gmail.com>
2025-11-20 19:22:58 +0530

    commit A file

--- A.java
+++ A.java
@@ -1,2 +1,34 @@
-"class A{}"
-"// updated" 
+public class Calculator {
+
+    // Addition
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    // Subtraction
+    public int subtract(int a, int b) {
+        return a - b;
+    }
+
+    // Multiplication
+    public int multiply(int a, int b) {
+        return a * b;
+    }
+
+    // Division
+    public int divide(int a, int b) {
+        if (b == 0) {
+            throw new IllegalArgumentException("Cannot divide by zero!");
+        }
+        return a / b;
+    }
+
+    public static void main(String[] args) {
+        Calculator calc = new Calculator();
+
+        System.out.println("Add: " + calc.add(10, 5));
+        System.out.println("Subtract: " + calc.subtract(10, 5));
+        System.out.println("Multiply: " + calc.multiply(10, 5));
+        System.out.println("Divide: " + calc.divide(10, 5));
+    }
+}
